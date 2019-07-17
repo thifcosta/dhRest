59dc761ad8c5890ad820421be23c07fe8df4591e
Thiago Fogagnoli Costa <thiferas@gmail.com>
2019-07-17 17:19:02 -0300

    layout e imagens ok

--- app/src/main/java/com/example/dh_rest/MainActivity.java
+++ app/src/main/java/com/example/dh_rest/MainActivity.java
@@ -41,34 +41,32 @@ public class MainActivity extends AppCompatActivity implements TadashiTypesListe
             }
         }
 
-        nomeDisplay.setText(userNome);
-        mailDisplay.setText(userEmail);
-
+        nomeDisplay.setText("Bem vindo, "+userNome+"!");
+        mailDisplay.setText("E-mail: "+userEmail);
 
         TadashiTypes tadashi01 = new TadashiTypes();
         tadashi01.setNome("Tadashi Junior");
-        tadashi01.setDefinition("Comida pronta em 10 minutos!");
+        tadashi01.setDefinition("Comida rápida,\npronta em 10 minutos!");
         tadashi01.setImagem(R.drawable.tada_01);
         listaDeTadashis.add(tadashi01);
 
         TadashiTypes tadashi02 = new TadashiTypes();
         tadashi02.setNome("Tadashi Senior");
-        tadashi02.setDefinition("Aquele banquete do cozinheiro\nexperiente!");
+        tadashi02.setDefinition("Banquete com\no vovô Tadashi!");
         tadashi02.setImagem(R.drawable.tada_02);
         listaDeTadashis.add(tadashi02);
 
         TadashiTypes tadashi03 = new TadashiTypes();
         tadashi03.setNome("Tadashi LatinLover");
-        tadashi03.setDefinition("Comidas afrodisíacas pra você\nconquistar a sua gata");
+        tadashi03.setDefinition("Aperitivos para\nimpressionar a sua gata");
         tadashi03.setImagem(R.drawable.tada_03);
         listaDeTadashis.add(tadashi03);
 
         TadashiTypes tadashi04 = new TadashiTypes();
         tadashi04.setNome("Tadashi GoodVibes");
-        tadashi04.setDefinition("Lanches pra você comer na praia,\ndepois de surfar!");
+        tadashi04.setDefinition("Lanches pra degustar\ndepois de pegar onda");
         tadashi04.setImagem(R.drawable.tada_04);
         listaDeTadashis.add(tadashi04);
-
 
         TadashiTypesAdapter tadashiTypesAdapter = new TadashiTypesAdapter(listaDeTadashis, this);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
---
+++ app/src/main/java/com/example/dh_rest/Model/ChefEspecialidades.java
@@ -0,0 +1,16 @@
+package com.example.dh_rest.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dh_rest.R;
+
+public class ChefEspecialidades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_especialidades);
+    }
+}
